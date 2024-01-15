3d2feb49031f772c32d77668a0d1b8c9259c4cb2
Deyvidas <devidbanyavichyus@yandex.ru>
2024-01-15 16:50:19 +0400

    Add realization of endpoints for get all Batch and OrderLine objs.

--- api/v1/routes/batch.py
+++ api/v1/routes/batch.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 from fastapi import status
 
+from config.sqlalchemy_config import session_config
+from core.batch.models import Batch
+from core.batch.repo import BatchRepoSqlAlchemy
+
 
 router = APIRouter(
     prefix='/batches',
@@ -12,6 +16,9 @@ router = APIRouter(
     path='/',
     status_code=status.HTTP_200_OK,
     description='Return all batches.',
+    response_model=list[Batch],
 )
 def get_list_of_all_batches():
-    return {'hello': 'world'}
+    with session_config.session() as session:
+        repo = BatchRepoSqlAlchemy(session)
+        return repo.get()
--- api/v1/routes/order.py
+++ api/v1/routes/order.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 from fastapi import status
 
+from config.sqlalchemy_config import session_config
+from core.order.models import OrderLine
+from core.order.repo import OrderLineRepoSqlAlchemy
+
 
 router = APIRouter(
     prefix='/orders',
@@ -12,6 +16,9 @@ router = APIRouter(
     path='/',
     status_code=status.HTTP_200_OK,
     description='Return all orders.',
+    response_model=list[OrderLine],
 )
 def get_list_of_all_orders():
-    return {'hello': 'world'}
+    with session_config.session() as session:
+        repo = OrderLineRepoSqlAlchemy(session)
+        return repo.get()
